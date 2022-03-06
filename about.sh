60ef4a69cc9ef4d42e32a0490c3b60e946040c22
danial6117 <73258340+danial6117@users.noreply.github.com>
2022-03-06 14:57:56 +0800

    Add files via upload

---
+++ about.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+clear
+echo -e "================================================="
+echo -e "#PREMIUM AUTOSCRIPT BY NIALSCRIPT"
+echo -e "================================================="
+echo -e "FOR UBUNTU 18.04 & UBUNTU 20.04 64 BIT"
+echo -e "FOR VPS WITH KVM AND VMWARE VIRTUALIZATION"
+echo -e "================================================="
+echo -e "OPENVPN && SSH"
+echo -e "TROJAN"
+echo -e "================================================="
