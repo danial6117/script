c8e7f49947921be5269ceb7c221de1c31164d3cb
danial6117 <73258340+danial6117@users.noreply.github.com>
2022-03-06 19:02:47 +0800

    Update trial.sh

--- trial.sh
+++ trial.sh
@@ -3,7 +3,7 @@ red='\e[1;31m'
 green='\e[0;32m'
 NC='\e[0m'
 MYIP=$(wget -qO- icanhazip.com);
-echo "Script By JoySmart"
+echo "NIALSCRIPT"
 clear
 IP=$(wget -qO- icanhazip.com);
 ssl="$(cat ~/log-install.txt | grep -w "Stunnel4" | cut -d: -f2)"
