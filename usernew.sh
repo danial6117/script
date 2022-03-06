a7cba158c8eb7a69b8c50a67618e1a4ca5031ca5
danial6117 <73258340+danial6117@users.noreply.github.com>
2022-03-06 14:16:50 +0800

    Add files via upload

---
+++ usernew.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+red='\e[1;31m'
+green='\e[0;32m'
+NC='\e[0m'
+MYIP=$(wget -qO- icanhazip.com);
+echo "NIALSCRIPT"
+clear
+read -p "Username : " Login
+read -p "Password : " Pass
+read -p "Expired (hari): " masaaktif
+
+IP=$(wget -qO- icanhazip.com);
+ssl="$(cat ~/log-install.txt | grep -w "Stunnel4" | cut -d: -f2)"
+sqd="$(cat ~/log-install.txt | grep -w "Squid" | cut -d: -f2)"
+ovpn="$(netstat -nlpt | grep -i openvpn | grep -i 0.0.0.0 | awk '{print $4}' | cut -d: -f2)"
+ovpn2="$(netstat -nlpu | grep -i openvpn | grep -i 0.0.0.0 | awk '{print $4}' | cut -d: -f2)"
+sleep 1
+clear
+useradd -e `date -d "$masaaktif days" +"%Y-%m-%d"` -s /bin/false -M $Login
+exp="$(chage -l $Login | grep "Account expires" | awk -F": " '{print $2}')"
+echo -e "$Pass\n$Pass\n"|passwd $Login &> /dev/null
+echo -e ""
+echo -e "==============================="
+echo -e "NIALVPN VVIP PREMIUM"
+echo -e "==============================="
+echo -e "USERNAME       : $Login "
+echo -e "PASSWORD       : $Pass"
+echo -e "==============================="
+echo -e "EXPIRED ON      : $exp"
+echo -e "==============================="
+echo -e "NIALSCRIPT"
