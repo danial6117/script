ff1da02469b7f5568f33b37bcd4dfe2387f81b91
danial6117 <73258340+danial6117@users.noreply.github.com>
2022-03-07 23:47:02 +0800

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -8,22 +8,20 @@ echo -e "==OPENVPN-SSH======================"
 echo -e ""
 echo -e "   1.NEW USER"  
 echo -e "   2.TRIAL USER"  
-echo -e "   3.HAPUS USER"     
-echo -e "   4.DELETE USER"               
+echo -e "   3.HAPUS USER"                
 echo -e ""
 echo -e "==TROJAN-TCP========================"
 echo -e ""
-echo -e "   5.NEW USER" 
-echo -e "   6.DELETE USER" 
+echo -e "   4.NEW USER" 
+echo -e "   5.DELETE USER" 
 echo -e ""
 echo -e "==ADD-ON============================"
 echo -e ""
-echo -e "   7.INFO SERVER"                        
-echo -e "   8.ALL USER MEMBER"        
-echo -e "   9.ALL USER ONLINE"    
-echo -e "   10.TCP&BBR TWEAK"    
-echo -e "   11.SPEEDTEST VPS "            
-echo -e "   12.REBOOT VPS "           
+echo -e "   6.INFO SERVER"                        
+echo -e "   7.ALL USER MEMBER"        
+echo -e "   8.ALL USER ONLINE"    
+echo -e "   9.TCP&BBR TWEAK"              
+echo -e "  10.REBOOT VPS "           
 echo -e ""
 echo -e "====================================="
 read -p "SELECT FROM OPTIONS [ 1 - 12 ] : " menu
@@ -38,30 +36,24 @@ trial
 hapus
 ;;
 4)
-delete
-;;
-5)
 tr-add
 ;;
-6)
+5)
 tr-del
 ;;
-7)
+6)
 info
 ;;
-8)
+7)
 member
 ;;
-9)
+8)
 cek
 ;;
-10)
+9)
 ./tcp.sh
 ;;
-11)
-speedtest
-;;
-12)
+10)
 reboot
 ;;
 *)
