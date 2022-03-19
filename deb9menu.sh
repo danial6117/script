b695b38a2e4faedcc3d339f05321514d2c833b89
danial6117 <73258340+danial6117@users.noreply.github.com>
2022-03-19 18:10:23 +0800

    Update deb9menu.sh

--- deb9menu.sh
+++ deb9menu.sh
@@ -1,24 +1,25 @@
 #!/bin/bash
 clear
 echo -e ""
-echo -e " ==NIALVPN-SERVER==================="
+echo -e "===================================="
+echo -e " ==NIALVPN-SERVER"
+echo -e "===================================="
 echo -e ""
-echo -e "==TROJAN-TCP======================="
-echo -e ""
+echo -e "===================================="
+echo -e "==TROJAN-TCP"
+echo -e "===================================="
 echo -e "   1.NEW USER" 
 echo -e "   2.DELETE USER" 
 echo -e "   3.START&CHANGE PORT" 
-echo -e ""
-echo -e "==ADD-ON==========================="
-echo -e ""
+echo -e "===================================="
+echo -e "==ADD-ON"
+echo -e "===================================="
 echo -e "   4.DNS CHANGER"   
 echo -e "   5.BBR TWEAK"   
 echo -e "   6.AUTOREMOVE"   
 echo -e "   7.REBOOT VPS "           
-echo -e ""
 echo -e "===================================="
-echo -e ""
-read -p "SELECT FROM OPTIONS [ 1 - 13 ] : " menu
+read -p "SELECT FROM OPTIONS [ 1 - 7 ] : " menu
 echo -e ""
 case $menu in
 1)
